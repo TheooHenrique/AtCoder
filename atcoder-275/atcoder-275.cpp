7e8866295d0f8dd2b73750be94d57b78a8e71225
luis-rochoso <sousaluislfs@gmail.com>
2026-03-03 13:48:54 -0300

    feat: 275

---
+++ atcoder-275/atcoder-275.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main(){
+  int n;
+  int maisAlto = 0;
+  int indiceMaisAlto = 0;
+
+
+  cin >> n;
+  for (int i = 0; i < n; ++i){
+    int altura;
+    cin >> altura;
+    if (altura > maisAlto){
+        indiceMaisAlto = i + 1;
+        maisAlto = altura;
+    }
+  }
+  
+  cout << indiceMaisAlto << endl;
+  
+  return 0;
+}
